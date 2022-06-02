11962d2d6b90481c744ebee7cf6d99e1758e32c4
happy happy <happygame1024@gmail.com>
2022-06-02 16:19:24 +0000

    更新 test.py

--- test.py
+++ test.py
@@ -9,4 +9,6 @@ import re
 from pymysql.converters import escape_string
 import os
 os.system("mysqldump -uroot -h127.0.0.1 -p0nishhishabi. -B mydb > backup_mydb.sql")
+
+
 # mysqldump -u 用户名 -p密码 -B 数据库1 数据库2 > 保存路径文件名.sql
